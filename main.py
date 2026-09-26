6ea9ad37b7083bd532a4897949199071a9fb05d6
unknown <gabrielr1457g412@gmail.com>
2026-09-25 21:42:50 -0300

    correção de validação de dados vazios no sistema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from persistencia import salvar_dados, carregar_dados
+import random
 
 # enumeração para os tipos de ativos
 class tipo_ativo(Enum):
@@ -30,12 +31,12 @@ ativosM = carregar_dados()
 ## funções
 def cadastro_ativo():
     print("\n --- Cadastrando Ativo ---")
-    if ativosM:
-        id_ativo = max(ativosM.keys()) + 1
-    else:
-        id_ativo = 1
-
-    name = input("Nome/hostname do ativo: ")
+    id_ativo = random.randint(1000, 9999)
+    while id_ativo in ativosM:
+        id_ativo = random.randint(1000, 9999)
+    name = input("Nome/hostname do ativo: ").strip()
+    while not name:
+        name = input("O nome não pode ser vazio. Digite novamente:").strip
     responsavel = input("Responsável pelo ativo: ")
     setor = input("Setor/localização do ativo: ")
 
